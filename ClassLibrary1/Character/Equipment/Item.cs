3d5180ff5c20c9f0cfbfded88cba5fa68c260036
Isaiah Murray <murray.isaiah13@gmail.com>
2023-11-13 16:56:06 -0500

    Created item class.

---
+++ ClassLibrary1/Character/Equipment/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Character.Equipment
+{
+    public enum TypeOfItem { Weapon, Armor, Shield, Trinket, Adventuring_Gear, Firearms, Explosives, Wondrous, Currency, Poison, Tools, Siege, Custom }
+    public class Item
+    {
+        public TypeOfItem ItemType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Add more properties as needed
+    }
+}
